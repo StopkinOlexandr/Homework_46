2511cca716f76ae97010a7399c52f2a5b1aef3bf
AlexandrStopkin <stnalex1@gmail.com>
2023-03-23 14:27:56 +0100

    Add Task2 add sort check in BookList

--- src/BookList.java
+++ src/BookList.java
@@ -8,11 +8,16 @@ public class BookList {
 
     arh.add(new Book("B", "bbb", 4));
     arh.add(new Book("A", "ccc", 5));
-    arh.add(new Book("A", "aaa", 3));
+    arh.add(new Book("A", "aab", 3));
     arh.add(new Book("B", "aaa", 3));
 
     arh.sort(new BookTitleAuthorComparator());
 //    Collections.sort(arh);
+    for (Book book : arh) {
+      System.out.println(book);
+    }
+
+    arh.sort(new BookPagesDownTitleComparator());
     for (Book book : arh) {
       System.out.println(book);
     }
