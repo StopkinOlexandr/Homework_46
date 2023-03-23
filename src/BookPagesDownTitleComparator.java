10d37618b5feb0b8f3a5593c272304bc98d60e10
AlexandrStopkin <stnalex1@gmail.com>
2023-03-23 14:26:13 +0100

    Add Task2 add solution BookPagesDownTitleComparator

--- src/BookPagesDownTitleComparator.java
+++ src/BookPagesDownTitleComparator.java
@@ -1,4 +1,23 @@
 import java.util.Comparator;
 
 public class BookPagesDownTitleComparator implements Comparator<Book> {
+
+  public BookPagesDownTitleComparator(){}
+
+  @Override
+  public int compare(Book book1, Book book2){
+
+    int pages1 = book1.getPages();
+    int pages2 = book2.getPages();
+    if(pages2 - pages1 != 0) {
+      return pages2 - pages1;
+    }
+
+    String title1 = book1.getTitle();
+    String title2 = book2.getTitle();
+    if (!title1.equals(title2)) {
+      return title1.compareTo(title2);
+    }
+    return 0;
+  }
 }
